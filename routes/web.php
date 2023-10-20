d964a0ff4db53ef87d00296e030a87230e88ce08
unknown <vchristando@gmail.com>
2023-10-21 01:34:34 +0700

    tambah controller dan route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Contoh Route di Laravel 8
+// Route::get('/Home', [PageController::class, 'home']);
