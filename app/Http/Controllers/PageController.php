74928f7f23ad65ff5d74a2b2d492e960858bc6cc
Kleponaeru <kelvinlie08@gmail.com>
2023-10-31 18:00:26 +0700

    First Commit

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function home()
+    {
+        return view("home");
+    }
+    public function register()
+    {
+        return view("register");
+    }
 }
