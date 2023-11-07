6c4cd407cbbd746c69ae6a737f7261257184efc4
Kleponaeru <kelvinlie08@gmail.com>
2023-11-07 17:33:39 +0700

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,12 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-class AuthController extends Controller
+class authController extends Controller
 {
-//   if (Auth::attempt($dataInput)){
-//     if(Auth::user()->role == "pengambil"){
-// return redirect('dashboard/pengambil');
-//   }
-// }
+    // view
+    public function login(){
+        return view('login');
+    }
+
+    // validate
+    public function validateLogin(Request $request){
+        // validate
+        $request -> validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        // validate input request
+        $datalogin = [
+            'email' => $request -> email,
+            'password' => $request -> password,
+        ];
+        if(Auth::attempt($datalogin)){
+            //hak akses
+            if(Auth::user()->role == 'peternak'){
+                return redirect('/dashboard/peternak');
+            } 
+            elseif(Auth::user()->role == 'pabrik'){
+                return redirect('/dashboard/pabrik');
+            }
+        }
+        else{
+            return redirect('/')->withErrors('Email atau Password tidak sesuai!');
+        }
+    }
+
+    // logout
+    public function logout(){
+        Auth::logout();
+        return redirect('/');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 
@@ -19,10 +20,25 @@ use App\Http\Controllers\PageController;
 // Route::get('/Home', [PageController::class, 'home']);
 
 
-// Route::middleware(['guest']) -> group(function (); {
+Route::middleware(['guest'])->group(function () {
+    Route::get('/', [PageController::class, 'home']);
+    Route::get('/login', [PageController::class, 'login']);
+    Route::get('/register', [PageController::class, 'register']);
+});
 
-// }
-Route::get('/', [PageController::class, 'home']);
-Route::get('/dashboard', [PageController::class, 'dashboard']);
-Route::get('/login', [PageController::class, 'login']);
-Route::get('/register', [PageController::class, 'register']);
+Route::middleware(['auth'])->group(function () {
+    Route::middleware(['cekrole:pengambil'])->group(function () {
+        Route::get('/dashboard/pengambil', [PageController::class, 'dashboardPengambil']);
+    });
+    Route::middleware(['cekrole:pemilik'])->group(function () {
+        Route::get('/dashboard/pemilik', [PageController::class, 'dashboardPengambil']);
+    });
+    Route::middleware(['cekrole:bank'])->group(function () {
+        Route::get('/dashboard/pemilik', [PageController::class, 'dashboardPengambil']);
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+});
+
+
+
